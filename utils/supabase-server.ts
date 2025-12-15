a4e27ca94d82c4ddc7a3e03baca5805dd65e67de
Yuberth <yuberth@gmail.com>
2025-12-15 15:10:21 -0500

    changes in the login method

--- utils/supabase-server.ts
+++ utils/supabase-server.ts
@@ -7,16 +7,14 @@ const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
 
 export function createServerSupabaseClient() {
-  const cookieStore = cookies() as any
-
   return createServerClient(supabaseUrl, supabaseAnonKey, {
     cookies: {
       getAll() {
-        return cookieStore.getAll()
+        return (cookies() as any).getAll()
       },
       setAll(cookiesToSet) {
         cookiesToSet.forEach(({ name, value, options }) =>
-          cookieStore.set(name, value, options)
+          (cookies() as any).set(name, value, options)
         )
       },
     },
